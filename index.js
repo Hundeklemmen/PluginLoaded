b1b43a9300bec2cd30730c8a26c44b371ca4f888
Alexander <32719411+Hundeklemmen@users.noreply.github.com>
2020-11-28 08:10:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 module.exports = function(PluginName, cb){
-    if(server.getPluginManager().getPlugin(PluginName).isEnabled()){
-        cb();
-        return;
-    } else {
-        var list = event.addListener("PluginEnableEvent", (eventResponse) => {
-            if(eventResponse.getPlugin().getName() == PluginName){
-                cb();
-                event.removeListener("PluginEnableEvent", list);
-                return;
-            }
-        });
-    }
+    if(server.getPluginManager().getPlugin(PluginName).isEnabled()) return cb();
+    
+    var list = event.addListener("PluginEnableEvent", (eventResponse) => {
+        if(eventResponse.getPlugin().getName() == PluginName){
+            cb();
+            event.removeListener("PluginEnableEvent", list);
+            return;
+        }
+   })
 }
